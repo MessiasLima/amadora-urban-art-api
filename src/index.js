4eea468977b74824d43f57a0590feb7a309b85db
Messias Junior <messiaslima.03@gmail.com>
2019-07-24 13:20:19 +0100

    feat: adding redirect to git repo page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const routes = require('./route')
 
 const app = express()
 const port = process.env.PORT || 3000
+const gitRepositoryURL = 'https://github.com/MessiasLima/amadora-urban-art-api'
 
 app.use('/api', routes)
+app.get('/', (request, response) => {
+    response.redirect(gitRepositoryURL)
+})
+
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`)
 })
